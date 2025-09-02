ebce94f920156f89ddc06cd6a78e75aaaf31d1d8
kaka590 <Vkaka4498@gmail.com>
2025-09-02 17:01:44 +0100

    Add files via upload

---
+++ gits.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.title('Sarah mokwe and Chinazorom')
+st.write('Just a quick application i created today')
+
+def code():
+    return 'Kaka'
+
+st.subheader('image')
+st.image(r"C:\Users\USER\OneDrive\Bilder\WhatsApp Image 2025-09-02 at 16.22.13_ed0dfb5d.jpg")
+st.subheader('image')
+st.image(r"C:\Users\USER\OneDrive\Bilder\WhatsApp Image 2025-09-02 at 16.22.04_5ac37136.jpg")
+st.subheader('audio')
+st.audio(r"C:\Users\USER\Music\Gunna-forever-be-mine-ft-Wizkid-(JustNaija.com).mp3")
+st.subheader('video')
+st.video(r"C:\Users\USER\Videos\Screen Recordings\Screen Recording 2025-06-09 131614.mp4")
